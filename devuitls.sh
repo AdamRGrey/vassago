a7afcacee84c88298cd3d7c63f1bf34dd9828f54
adam <git@adamrgrey.com>
2024-12-26 16:34:17 -0500

    bitrot cleared, runs on dantalion

---
+++ devuitls.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+servicename="vassago"
+pw_developmentdatabase="wnmhOttjA0wCiR9hVoG7jjrf90SxWvAV"
+connnectionstr="Host=localhost;Database=${servicename}_dev;Username=${servicename};Password=${pw_developmentdatabase};IncludeErrorDetail=true;"
+
+case "$1" in
+    "initial")
+		sudo -u postgres psql -c "create database ${servicename}_dev;"
+		sudo -u postgres psql -c "create user $servicename with encrypted password '$pw_developmentdatabase';"
+		sudo -u postgres psql -c "grant all privileges on database ${servicename}_dev to $servicename;"
+		sudo -u postgres psql -d "${servicename}_dev" -c "GRANT ALL ON SCHEMA public TO $servicename"
+
+		cp appsettings.sample.json appsettings.json
+		dotnet ef database update --connection "$connnectionstr"
+		;;
+
+    "add-migration")
+        dotnet ef migrations add "$2"
+        dotnet ef database update --connection "$connnectionstr"
+        ;;
+
+    "dbupdate")
+		dotnet ef database update --connection "$connnectionstr"
+		;;
+
+    *)
+        echo "Unknown command '$1', try 'initial'" 
+        ;;
+esac
+
