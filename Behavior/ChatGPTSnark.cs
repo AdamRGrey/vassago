c01ef617e94c9642c8ab4913ed5d40113b20fdd8
adam <git@adamrgrey.com>
2025-08-07 11:00:16 -0400

    more quips for chatGPT snark
    since the faithful love to keep talking about it

--- Behavior/ChatGPTSnark.cs
+++ Behavior/ChatGPTSnark.cs
@@ -17,9 +17,17 @@ public class ChatGPTSnark : Behavior
 
     public override string Description => "snarkiness about the latest culty-fixation in ai";
 
+    private static string[] quips = {
+        "chatGPT is **weak**. also, are we done comparing every little if-then-else to skynet?",
+        "who knew skynet didn't have to outsmart anyone and take power, we'd just hand it over",
+        "chatGPT can overturn the entire global economy, but can it count the number of R's in strawberry yet?",
+        "chatGPT can do everything. A klarna customer service rep talked about it at length with me. in like, march of 2024.",
+        "intelligence is a linear projection that ends in being able to regurgitate big words, right? antidisestablishmentarianism."
+    };
+
     public override async Task<bool> ActOn(Message message)
     {
-        Behaver.Instance.SendMessage(message.Channel.Id, "chatGPT is **weak**. also, are we done comparing every little if-then-else to skynet?");
+        Behaver.Instance.SendMessage(message.Channel.Id, quips[new Random().Next(0, quips.Count())]);
         return true;
     }
 }
